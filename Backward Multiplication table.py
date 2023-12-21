effe8e7380752b0d4f6b7bd3c3ed52426a94651a
Tumit Dass <146959253+Tes-miT@users.noreply.github.com>
2023-12-21 20:16:01 +0530

    Backward Multiplication Table
    #LIVE
    
    Wrote a code to print out the multiplication table (from x1 to x10) of any inputted number by the user, in reverse order (from x10 to x1).

---
+++ Backward Multiplication table.py
@@ -0,0 +1,3 @@
+num = int(input("Enter your desired number : ")) #Asking the user for a number that they want the table of
+for i in range(10,0,-1): #using for loop to run until multiplicator shows 1 from 10
+    print(f"{num} x {i} = ", num*i) #Printing it out to show the table
