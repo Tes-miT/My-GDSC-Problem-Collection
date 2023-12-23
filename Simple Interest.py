c6076093c481adfb5f91904fde65976de306fd98
Tumit Dass <146959253+Tes-miT@users.noreply.github.com>
2023-12-23 16:07:25 +0530

    Simple Interest
    #HOMEWORK
    
    Wrote a code to calculate the simple interest by inputted Principal amount, Tenure and Rate or Interest.

---
+++ Simple Interest.py
@@ -0,0 +1,4 @@
+P = int(input("Please enter your principal amount : "))
+T = int(input("Please enter the tenure : "))
+R = int(input("Please enter your interest rate : "))
+print("Your simple interest will be =", (P*T*R)/100)
